c0891875e9d54c809125ce185b4717a0ca2c6cae
iaadi4 <adityasingh40675@gmail.com>
2025-10-21 21:48:00 +0530

    fix: homepage not loading, style: improve homepage ui

--- src/main.rs
+++ src/main.rs
@@ -18,13 +18,6 @@ fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let buf_reader = BufReader::new(&stream);
-    let _http_request: Vec<_> = buf_reader // collects the lines of request send by browser
-        .lines() // return iterators after spliting data when it sees newline
-        .map(|result| result.unwrap()) // unwraps result, error if data it not valid utf-8
-        .take_while(|line| !line.is_empty()) // iterates over unwrapped result till line is empty
-        .collect(); // makes all request store in vector
-
     let (status_line, content_path) = if request_line == "GET / HTTP/1.1" {
         ("HTTP/1.1 200 OK", "public/hello.html")
     } else {
