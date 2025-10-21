df566d85a6b7325afc6b6aa7f4a19e9c17835a1f
iaadi4 <adityasingh40675@gmail.com>
2025-10-21 21:38:49 +0530

    feat: add response for not-found

--- src/main.rs
+++ src/main.rs
@@ -16,14 +16,22 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let buf_reader = BufReader::new(&stream);
     let _http_request: Vec<_> = buf_reader // collects the lines of request send by browser
         .lines() // return iterators after spliting data when it sees newline
         .map(|result| result.unwrap()) // unwraps result, error if data it not valid utf-8
         .take_while(|line| !line.is_empty()) // iterates over unwrapped result till line is empty
         .collect(); // makes all request store in vector
 
-    let status_line = "HTTP/1.1 200 OK";
-    let contents = fs::read_to_string("public/hello.html").unwrap();
+    let (status_line, content_path) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "public/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "public/404.html")
+    };
+
+    let contents = fs::read_to_string(content_path).unwrap();
     let contents_length = contents.len();
 
     let response = format!("{status_line}\r\nContent-Length: {contents_length}\r\n\r\n{contents}");
