dfc9e1c4482ad4e7b79cb2c3ed2f70f260790985
iaadi4 <adityasingh40675@gmail.com>
2025-10-21 20:58:04 +0530

    feat: add basic html response

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::BufReader;
 use std::net::TcpListener;
 use std::net::TcpStream;
@@ -15,13 +16,17 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let http_request: Vec<_> = buf_reader // collects the lines of request send by browser
+    let _http_request: Vec<_> = buf_reader // collects the lines of request send by browser
         .lines() // return iterators after spliting data when it sees newline
         .map(|result| result.unwrap()) // unwraps result, error if data it not valid utf-8
         .take_while(|line| !line.is_empty()) // iterates over unwrapped result till line is empty
         .collect(); // makes all request store in vector
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("public/hello.html").unwrap();
+    let contents_length = contents.len();
 
+    let response = format!("{status_line}\r\nContent-Length: {contents_length}\r\n\r\n{contents}");
+    
     stream.write_all(response.as_bytes()).unwrap();
 }
